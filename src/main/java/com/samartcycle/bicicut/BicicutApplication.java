25f286775384b79bbc4f9b8db512334a61df1dbc
Ernesto Fierro <hatuey.fierro@gmail.com>
2024-04-21 00:38:53 -0600

    Primer commit para inicializar el proyecto

---
+++ src/main/java/com/samartcycle/bicicut/BicicutApplication.java
@@ -0,0 +1,13 @@
+package com.samartcycle.bicicut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicutApplication.class, args);
+	}
+
+}
